cc67229be18d1c47fb55f40308dd5fd90039f420
takagi <lianosjp010@gmail.com>
2019-07-16 17:01:39 +0900

    FragmentShaderの実験

---
+++ ShaderStudy-Legacy/Assets/Contents/Scripts/Experiments/ShowFragmentInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowFragmentInfo : MonoBehaviour
+{
+    [SerializeField]
+    Renderer targetA;
+
+    [SerializeField]
+    Renderer targetB;
+
+    int propId;
+
+    private MaterialPropertyBlock mpbA, mpbB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mpbA = new MaterialPropertyBlock();
+        mpbB = new MaterialPropertyBlock();
+
+        propId = Shader.PropertyToID("_DestructionValue");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(targetA.material.GetFloat(propId));
+        Debug.Log(targetB.material.GetFloat(propId));
+    }
+}
